c8348a3f073749d82b907a7ceac980c7acbdf894
wen <wen.sima@gmail.com>
2024-11-01 17:47:54 +1100

    add talwind to some pages

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,33 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
+//console.log(josefin);
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description: "Luxurious cabin hotel, located in the heart of the Australia",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-900 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7x mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
