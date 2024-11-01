592fafaee4e9812dea89c4847dbd1e419f314217
wen <wen.sima@gmail.com>
2024-11-01 15:38:05 +1100

    initial

---
+++ app/_components/Counter.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ user }) {
+  const [count, setCount] = useState(0);
+  // console.log(user);
+  return (
+    <div>
+      <p>there are {user.length} users</p>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+    </div>
+  );
+}
