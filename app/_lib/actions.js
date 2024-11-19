22dae5069c977690bbe0484306c2e6238ba67306
wen <wen.sima@gmail.com>
2024-11-19 11:49:36 +1100

    delete reservation function

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,9 +1,55 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+
+export async function updateGuest(formData) {
+  // console.log(formData);
+  const session = await auth();
+  if (!session) {
+    throw new Error("You must be logged in to update your profile");
+  }
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error("please provide a valid national ID");
+  }
+  const updateData = { nationality, countryFlag, nationalID };
+  //console.log(updateDate);
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    throw new Error("Guest could not be updated");
+  }
+  revalidatePath("/account/profile");
+}
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("You must be logged in to delete a reservation");
+  }
+  const guestBookings = await getBookings(session.user.guestId);
+  const bookingIds = guestBookings.map((booking) => booking.id);
+  if (!bookingIds.includes(bookingId)) {
+    throw new Error("You are not authorized to delete this reservation");
+  }
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("/account/reservations");
 }
